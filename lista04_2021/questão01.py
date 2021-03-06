f2f2d2d358de5628e0d3cfd32f3a644c4ff4135c
Jose Alfredo <42855292+chandocan@users.noreply.github.com>
2021-03-06 14:37:11 -0300

    Update questão01.py

--- lista04_2021/questão01.py
+++ lista04_2021/questão01.py
@@ -12,15 +12,25 @@ list_prioritario = []
 for i in range(20): # 20
     tipo = input('tipo do cliente n ou p:')
     nome = input('nome do cliente :')
-
+    # registros do clientes
     if tipo == 'n':
         list_normal.append(nome)
     if tipo == 'p':
         list_prioritario.append(nome)
+# para add os clientes de forma alternada
+# para clientes em espaço 20
+# liste a sequência de clientes, sempre puxando um cliente 
+# normal e um prioritário
 for clientes in range(20):
+    # precisou ler o tamanho da fila para pode colocar na lista total
+    # pois daria um erro de lista inexistente
+    # o tanho da lista diferente de zero
+    # vai puxa sempre o primeiro de cada lista anterior
     if len(list_normal) != 0:
+        # add em  total o primeiro de normal
         total_de_clientes.append(list_normal.pop(0))
     if len(list_prioritario) != 0:
+        # add em total o primeiro de prioritario
         total_de_clientes.append(list_prioritario.pop(0))
-    
+# todos clientes de forma altendida entre 'normal' e 'prioritario'  
 print(total_de_clientes)
